fd9741ffdf86c6920a0de72a9ae7bd468da226df
AdityaVikram8 <aditya0247.becse24@chitkara.edu.in>
2025-10-25 13:26:07 +0530

    Added contact us page

--- jobs/job-list.js
+++ jobs/job-list.js
@@ -25,5 +25,55 @@ const jCategory=[
         title:"Buissness Associate",
         rate:"$900-1200/m",
         av:"Part Time",
+        companyName:"Uber",
+        location:"Paris",
+        vacancy:"05",
+        hours:"50h / Week",
+        description: `As a Web Developer at Google, you will design and develop dynamic web applications that
+         scale to millions of users. Responsibilities include creating responsive interfaces, writing clean 
+         and efficient code, optimizing site performance, and ensuring cross-browser compatibility. You will 
+         collaborate closely with product managers, designers, and backend engineers to deliver world-class user
+          experiences.`,
+        workplace:"Work from home",
+        education:"Bachelor degree in computer science Courses",
+        experience:"2 to 5 years",
+    },
+    {
+        index:2,
+        image:"../images/linkedin.png",
+        title:"Frontend Developer",
+        rate:"$90-1200/m",
+        av:"Part Time",
+        companyName:"Linkedin",
+        location:"London",
+        vacancy:"03",
+        hours:"30h / Week",
+        description: `As a Web Developer at Google, you will design and develop dynamic web applications that
+         scale to millions of users. Responsibilities include creating responsive interfaces, writing clean 
+         and efficient code, optimizing site performance, and ensuring cross-browser compatibility. You will 
+         collaborate closely with product managers, designers, and backend engineers to deliver world-class user
+          experiences.`,
+        workplace:"Work from home",
+        education:"Bachelor degree in computer science Courses",
+        experience:"2 to 5 years",
+    },
+    {
+        index:3,
+        image:"../images/instagram.png",
+        title:"Product Designer",
+        rate:"$8000-12000/m",
+        av:"Part Time",
+        companyName:"Instagram",
+        location:"Australia",
+        vacancy:"100",
+        hours:"200h / Week",
+        description: `As a Web Developer at Google, you will design and develop dynamic web applications that
+         scale to millions of users. Responsibilities include creating responsive interfaces, writing clean 
+         and efficient code, optimizing site performance, and ensuring cross-browser compatibility. You will 
+         collaborate closely with product managers, designers, and backend engineers to deliver world-class user
+          experiences.`,
+        workplace:"Work from home",
+        education:"Bachelor degree in computer science Courses",
+        experience:"2 to 5 years",
     },
 ]
---
+++ send_email.php
@@ -0,0 +1,24 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=='POST'){
+    $name=$_POST['name'];
+    $name=$_POST['email'];
+    $name=$_POST['message'];
+
+    $to="example@email.com";
+    $subject="New contact form submission";
+    $headers='From: ' .$name . "<" .$email .
+    ">"\r\n;
+    $headers .="Reply-To: " . $email . "\r\n";
+    $headers .="Content-Type: text/plain; charset=utf-8 \r\n";
+    if(mail($to, $subject,$message,$headers)){
+        http_response_code(200);
+        echo "Email sent successfully!";
+    }else{
+        http_response_code(500);
+        echo "OOPS!  Something went wrong";
+    }
+}else{
+    http_response_code(403);
+    echo "Access denied";
+}
+?>
